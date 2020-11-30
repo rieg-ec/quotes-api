2f646af18ba53f14cdc3f6b25737ce9b89ee28f7
rieg-ec <rieg@uc.cl>
2020-11-30 18:55:10 -0300

    fix: remove port from knexfile

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -1,15 +1,12 @@
-// Update with your config settings.
 require('dotenv').config({ path: '../../.env' });
 
 const HOST = process.env.POSTGRES_HOST || 'localhost';
-const PORT = process.env.POSTGRES_PORT || 5432;
 
 module.exports = {
   development: {
     client: 'pg',
     connection: {
       host: HOST,
-      port: PORT,
       database: process.env.POSTGRES_DB,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
